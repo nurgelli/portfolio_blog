acda0d04c52d35afd03727e6de0e7a1f6975a090
nurgelli <nt.interpretor@gmail.com>
2025-06-19 15:25:19 +0500

    base.py in settings folder gives error, thats why removed to parent dir

--- about/models.py
+++ about/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class About(models.Model):
+    name = models.CharField(max_length=64)
+    about = models.TextField(default='null')
+    image = models.ImageField(default='null')
+    technologies = models.CharField()
+    
+    def __str__(self):
+        return self.name
--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import About
 # Create your views here.
+
+def about(request):
+    context = About.objects.all()
+    
+    return render(request, 'about/index.html', {'context': context})
