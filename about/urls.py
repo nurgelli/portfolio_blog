d0bea80fbcf81889e16ebb5a79a588657e01715b
nurgelli <nt.interpretor@gmail.com>
2025-06-20 13:13:22 +0500

    added about page

--- about/urls.py
+++ about/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.about, name='about')
+    path('', views.ListView.as_view(), name='about')
 ]
--- about/views.py
+++ about/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import About
 # Create your views here.
 
-def about(request):
-    context = About.objects.all()
+# def about(request):
+#     context = About.objects.all()
     
-    return render(request, 'about/index.html', {'context': context})
+#     return render(request, 'about/index.html', {'context': context})
+
+class About(ListView):
+    model = About
+    template_name = 'about/index.html'
+    context = 'context'
+    
