7c3c7dd116859b4d2728aa61219e96db1a183c31
nurgelli <nt.interpretor@gmail.com>
2025-07-09 00:43:37 -0700

    new apps were open

--- job/models.py
+++ job/models.py
@@ -8,15 +8,18 @@ class Job(models.Model):
     start_date = models.DateField(blank=True, null=True)
     due_date = models.DateField(blank=True, null=True)
     
+    
     def __str__(self):
-        return self.name
-    
-    
+        return self.title
     
 class Location(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE),
-    location = models.CharField(max_length=64)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='jobs', null=True)
+    location = models.CharField(max_length=256, choices=[
+        ('ashgabat', 'Ashgabat'),
+        ('mary', 'Mary'),
+        ('dashoguz', 'Dashoguz'),
+    ], default='Ashgabat', null=False, blank=False)
     
     def __str__(self):
-        return f'{self.location}'
+        return f'{self.location} > {self.job.title}'
 
